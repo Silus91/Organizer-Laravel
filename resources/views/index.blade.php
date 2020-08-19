6ba4740d96b8da4a510080ce68f5cb8dea193fcc
silus91 <wizard91@o2.pl>
2020-08-19 21:57:34 +0100

    index blade editing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+                    todo organize this
                 </div>
             </div>
         </div>
