b340f4ce7b1384a55437dcf1d026f894670baa0c
silus91 <wizard91@o2.pl>
2020-08-24 22:19:15 +0100

    atribute removed

--- app/Task.php
+++ app/Task.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
-
     public function collection()
     {
        return $this->belongsTo(\App\Collection::class);
